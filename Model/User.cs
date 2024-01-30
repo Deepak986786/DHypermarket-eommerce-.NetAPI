cac557ff892f16509917b3b5e180a69b6f66d4da
Deepak <57728553+Deepak986786@users.noreply.github.com>
2024-01-30 20:32:28 +0530

    changed model

--- Model/User.cs
+++ Model/User.cs
@@ -9,5 +9,6 @@ public class User
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public List<Product> CartItems {get; set; }
 
 }
